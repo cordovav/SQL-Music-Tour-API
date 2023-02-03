670b82220289da3a04ff1231b31cab7b3af26f70
Veny Cordova <venycordova@gmail.com>
2023-02-02 19:39:18 -0500

    adding routes and controllers to bands, events, and stages

---
+++ models/controllers/events_controller.js
@@ -0,0 +1,79 @@
+//dependencies
+const events = require('express').Router()
+const db = require('..')
+const { Events } = db
+
+//find all events
+events.get('/', async (req,res) => {
+    try {
+        const foundEvents = await Events.findAll({
+            order: [ [ 'available_start_time', 'ASC' ] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
+        res.status(200).json(foundEvents)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    })
+
+
+events.get('/:id', async (req,res) => {
+    try {
+        const foundEvents = await Events.findAll({
+            where: { event_id: req.params.id }
+        })
+        res.status(200).json(foundEvents)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    })
+
+//create a band
+events.post('/', async (req,res) => {
+    try {
+        const newEvent = await Events.create(req.body)
+        res.status(200).json({
+            message: "successfully inerted new event",
+            data: newEvent
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    })
+
+// UPDATE A BAND
+events.put('/:id', async (req, res) => {
+    try {
+        const updatedEvents = await Events.update(req.body, {
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedEvents} event(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A BAND
+events.delete('/:id', async (req, res) => {
+    try {
+        const deletedEvents = await Events.destroy({
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedEvents} event(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//export
+module.exports = events;
